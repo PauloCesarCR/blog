bc8925d03e7e29b8e31e6cb960f4616fdaa92783
Paulo Cesar <paulo.santos@intelitrader.com.br>
2023-05-26 12:32:49 -0300

    diminui distancia entre o search e os posts

--- src/components/SearchPost/SearchPost.Styles.ts
+++ src/components/SearchPost/SearchPost.Styles.ts
@@ -6,7 +6,7 @@ export const ContainerSearch = styled.div`
     align-items: center;
     justify-content: center;
     margin-top: 30px;
-    margin-bottom: 40px;
+    margin-bottom: 20px;
 `
 export const InputSearch = styled.input`
     width: 50%;
