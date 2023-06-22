999ee36ca1ab61b72c939e9167e4d5bae6f24a36
Paulo Cesar <paulo.santos@intelitrader.com.br>
2023-06-21 21:09:08 -0300

    corrige metade do bug do tema

--- src/pages/Post/PostDetails.Styles.ts
+++ src/pages/Post/PostDetails.Styles.ts
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
 export const PostContainer = styled.div`
-width: 100%;
-height: 100%;
-position: relative;
+height: 100vh;
 
 `
 export const Post= styled.div`
