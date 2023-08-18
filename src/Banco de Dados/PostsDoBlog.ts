d49444bd8d527e4b94d4ec378e901df1ed88157d
Paulo Cesar <paulo.santos@intelitrader.com.br>
2023-08-18 12:04:03 -0300

    new post

--- src/Banco de Dados/PostsDoBlog.ts
+++ src/Banco de Dados/PostsDoBlog.ts
@@ -1,16 +1,17 @@
 import imgDinizismo from '../assets/dinizismo.png'
 import jonas from '../assets/jonas.png'
+import rotative from '../assets/rotative.png'
 
 
 const posts = [
-    {
-      id: 1,
-      title: 'Programar é fácil?',
-      type: 'programation',
-      date: '07/03/2023',
-      image: '',
-      image_description: '',
-      description:`Definitivamente programar não é fácil. É completamente o oposto do que dizem na internet por aí.
+  {
+    id: 1,
+    title: 'Programar é fácil?',
+    type: 'programation',
+    date: '07/03/2023',
+    image: '',
+    image_description: '',
+    description: `Definitivamente programar não é fácil. É completamente o oposto do que dizem na internet por aí.
       | "6 meses de curso e sálario acima de 5k". |
       É o que dizem os  gurus da internet, mas como diz o Téo José quando um jogador faz algo bizarro "NÃOOO, NÃO É ASSIM".
       Trabalhar sentado, home-office, que maravilha em? Mas e os contras? existem nesse mundo?
@@ -19,77 +20,86 @@ const posts = [
       É preciso estar concentrado quase 100% do seu tempo, escrever um bom código exige concentração e boa lógica, Se livrar de bugs exige paciência e emocional.
       | Programar é um eterno, "Eu não sei, mas eu aprendo".`,
 
-    },
-    {
-      id: 2,
-      title: 'Dai a Deus o que é de Deus',
-      type: 'Religious',
-      date: '08/03/2023',
-      image: '',
-      image_description: '',
-      description: `Estava fazendo meu devocional hoje pela manhã e é incrível como Deus faz tudo de forma perfeita. | 
+  },
+  {
+    id: 2,
+    title: 'Dai a Deus o que é de Deus',
+    type: 'Religious',
+    date: '08/03/2023',
+    image: '',
+    image_description: '',
+    description: `Estava fazendo meu devocional hoje pela manhã e é incrível como Deus faz tudo de forma perfeita. | 
       Em Malaquias 3:10, Deus diz "Trazei todos os dízimos à casa do tesouro, para que haja mantimento na minha casa, e depois fazei prova de mim nisto, diz o Senhor dos Exércitos, se eu não vos abrir as janelas do céu, e não derramar sobre vós uma bênção tal até que não haja lugar suficiente para a recolherdes." |
       Deus pede somente parte do ele já te deu, ele não pede o que você não tem. E diz o SENHOR dos Exércitos, derramarei chuva de bençãos sobre vós. diferente de outros Deuses citados na bíblia que pediam sacrifícios ( absurdos, digasse) ANTES de oferecer "bençãos". |
       O grande Eu Sou diz a nós, eu te abençoou meu filho, matenha meu templo, e nada te faltará.
       Mas, buscai primeiro o reino de Deus, e a sua justiça, e todas estas coisas vos serão acrescentadas. | Mateus 6:33
       `
-    },
-    {
-      id: 3,
-      title: 'Dinizismo no Fifa?',
-      type: 'Gaming',
-      date: '13/03/2023',
-      image: imgDinizismo,
-      image_description: `Aproximação em apenas um setor do campo de ataque (toqueira) e a chegada do Walker pelo lado direito, o que deixa meu time extremamente exposto a contra-ataques, por isso é de extrema importância a manutenção da posse de bola para vencer :)
+  },
+  {
+    id: 3,
+    title: 'Dinizismo no Fifa?',
+    type: 'Gaming',
+    date: '13/03/2023',
+    image: imgDinizismo,
+    image_description: `Aproximação em apenas um setor do campo de ataque (toqueira) e a chegada do Walker pelo lado direito, o que deixa meu time extremamente exposto a contra-ataques, por isso é de extrema importância a manutenção da posse de bola para vencer :)
      a escolha pelo city foi pela característica do jogadores que são de toque e mobilidade.`,
-      description: `Quem me conhece sabe que eu sou extremamente apaixonado por futebol e jogar fifa nas horas vagas é bão demais. E enquanto eu jogava me veio a idéia de jogar no modo "Dinizismo", entrando mais na parte tática do futebol (o famoso estilo funcional), eu semprei desse estilo de jogo ( APESAR de achar o posicional mais efetivo ), aproximação, toco e me voy etc. E então eu pensei  e se eu implementasse o dinizismo no Fifa?
+    description: `Quem me conhece sabe que eu sou extremamente apaixonado por futebol e jogar fifa nas horas vagas é bão demais. E enquanto eu jogava me veio a idéia de jogar no modo "Dinizismo", entrando mais na parte tática do futebol (o famoso estilo funcional), eu semprei desse estilo de jogo ( APESAR de achar o posicional mais efetivo ), aproximação, toco e me voy etc. E então eu pensei  e se eu implementasse o dinizismo no Fifa?
       Eu não sou fã do Dinizismo, ok? kkkkk
       | E eis o resultado:      
       `
-    },
+  },
 
-    {
-      id: 4,
-      title: 'Pronunciando a palavra World em inglês',
-      type: 'Study',
-      date: '20/03/2023',
-      image:'' ,
-      image_description: '' ,
-      description: `Hoje estava estudando inglês e vi uma dica de pronúncia da palavra World em inglês, onde simplesmente nós fazemos a junção de Where + Old.  
+  {
+    id: 4,
+    title: 'Pronunciando a palavra World em inglês',
+    type: 'Study',
+    date: '20/03/2023',
+    image: '',
+    image_description: '',
+    description: `Hoje estava estudando inglês e vi uma dica de pronúncia da palavra World em inglês, onde simplesmente nós fazemos a junção de Where + Old.  
       Achei top a dica visto que é uma palavrinha que embola a lingua na hora de falar e  que a galera (inclusive eu) costuma ter dificuldade para pronunciar kkkk
       `
-    },
+  },
 
-    {
-      id: 5,
-      title: 'A minha vontade x A vontade de Deus',
-      type: 'Religious',
-      date: '17/04/2023',
-      image:jonas ,
-      image_description: `Aqui Jonas não amou. Ele simplesmente não amou o seu próximo e nem a Deus. 
+  {
+    id: 5,
+    title: 'A minha vontade x A vontade de Deus',
+    type: 'Religious',
+    date: '17/04/2023',
+    image: jonas,
+    image_description: `Aqui Jonas não amou. Ele simplesmente não amou o seu próximo e nem a Deus. 
       O que aprendemos sobre isso? Nós precisamos amar a Deus, amar o próximo e estar ligados a Ele para que as nossas vontades sejam a vontades Dele. como diz Paulo:
       Assim, já não sou eu quem vive, mas Cristo vive em mim. Gálatas 2:20 `,
-      description: `Ao decorrer da história, temos varias histórias sobre como Deus interfere no nosso livre arbitrío quando é necessário.
+    description: `Ao decorrer da história, temos varias histórias sobre como Deus interfere no nosso livre arbitrío quando é necessário.
       Posso citar aqui diversas histórias, como a de Jonas por exemplo. Deus ordena a Jonas a viajar a Nínive para alertar o povo sobre o pecado deles e que seriam castigados caso não deixassem as más praticas de lado. Em vez disso, Jonas pegou um barco para Társis, fugindo do que Deus ordenou a ele. | A história relata que uma tempestade começou e Jonas pediu para ser lançado ao mar, para que o mar se acalmasse e os tripulantes pudessem seguir viajem tranquilamente, o mar se acalmou e Deus enviou um grande peixe para engolir Jonas. Jonas sabia que tinha errado em sua escolha, por ódio no coração contra os Nínivitas escolheu fugir da presença de Deus e de sua vontade.  O amor de Deus pelos pecadores ninivitas fez com que ele interferisse na vontade de Jonas. Deus queria que Jonas pregasse o arrependimento ao povo de Ninive a fim de salva-los. E assim aconteceu, após clamar ao Senhor do fundo do oceano e ser devolvido a Terra pelo grande peixe, Jonas foi a Nínive e pregou, o povo se arrependeu de seus erros e Deus removeu a sentença de morte que tinha lançado sobre eles. | O ódio de Jonas contra Nínive questionou a vontade de Deus em duas situações:
       `
-    },
+  },
 
-    {
-      id: 6,
-      title: 'Transpose do Blog para Typescript + Styled Components',
-      type: 'Programation',
-      date: '26/05/2023',
-      image:'' ,
-      image_description: ``,
-      description: `É incrível como o Typescript deixa tudo mais limpo e organizado, ainda estou estudando alguns types para me livrar o bendito "Any", mas já da pra notar que tudo fica mais claro e limpo. | 
+  {
+    id: 6,
+    title: 'Transpose do Blog para Typescript + Styled Components',
+    type: 'Programation',
+    date: '26/05/2023',
+    image: '',
+    image_description: ``,
+    description: `É incrível como o Typescript deixa tudo mais limpo e organizado, ainda estou estudando alguns types para me livrar o bendito "Any", mas já da pra notar que tudo fica mais claro e limpo. | 
       Sobre o Styled-Components, eu sempre vejo nos requisitos das vagas e resolvi dá uma olhada e que mão na roda em. Trabalhar com componentes que podem ser estilizados através de Props é extremamente sensacional e ainda deixa o código bem mais coeso. | 
       Eu passei algum tempo ( 1 hora ) para implementar o Toogle Theme com o styled-components e foi aí que descobri o ThemeProvider da própria lib, outra mão na roda. |
       Eu tenho um ThemeProvider envolvendo as minhas rotas no App.tsx, um GlobalStyle e foi só passar a função Toogle para as duas rotas e gg, lá eu troco o theme. Ficou um código meio duplicado, ainda estou vendo um jeito de resolver isso, mas tá funcionando. |
       Aqui tem tudo que você precisa para estudar Styled-Components : |  
       https://styled-components.com/docs
       `
-    },
-    
-  ]
-  
-  export default posts;
+  },
+  {
+    id: 7,
+    title: 'Verificar se uma string é rotativa em JS',
+    type: 'Programation',
+    date: '18/08/2023',
+    image: rotative,
+    image_description: ``,
+    description: ``
+  },
+
+]
+
+export default posts;
